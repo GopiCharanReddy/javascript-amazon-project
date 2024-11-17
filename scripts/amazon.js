939671b367bd37cfd61c9f63cc0bb96cc8c94447
GopiCharanReddy <gopicharanvgcr@gmail.com>
2024-11-17 16:48:40 +0530

    Add quantity selector using dom

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -56,18 +56,21 @@ document.querySelector('.js-products-div').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
 button.addEventListener('click',()=>{
 const productId= button.dataset.productId;
+
 let matchingItem;
 cart.forEach((item) => {
   if(productId===item.productId){
     matchingItem=item;
   }
 });
+const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+const quantity = Number(quantitySelector.value);
 if(matchingItem){
-  matchingItem.quantity+=1;
+  matchingItem.quantity+=quantity;
 }else{
   cart.push({
     productId : productId,
-    quantity : 1
+    quantity : quantity
   });
 }
 let cartQuantity =0;
