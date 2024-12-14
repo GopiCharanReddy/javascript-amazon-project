46f1b0dee9b4587f7b5f07417be08ede74576da8
GopiCharanReddy <gopicharanvgcr@gmail.com>
2024-12-14 23:30:55 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,28 +5,20 @@ import {
   updateQuantity,
   updateDeliveryOption
 } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+
 export function renderOrderSummary(){
 let cartSummary = "";
 cart.forEach((cartItem) => {
-  let matchingProduct;
   const productId = cartItem.productId;
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
+  const matchingProduct = getProduct(productId);
   const deliveryOptionsId = cartItem.deliveryOptionsId;
 
-  let deliveryOption;
-  deliveryOptions.forEach((option) => {
-    if (option.id === deliveryOptionsId) {
-      deliveryOption = option;
-    }
-  });
+  const deliveryOption = getDeliveryOption(deliveryOptionsId);
+  
   const today = dayjs();
   const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dataString = deliveryDate.format("dddd, MMMM D");
