55c94aba0d1c59467cecee9b62979dcffbe20cc3
GopiCharanReddy <gopicharanvgcr@gmail.com>
2024-12-24 00:41:19 +0530

    testing the ordersummary file

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,6 +1,5 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-
 export const deliveryOptions = [{
   id :'1',
   deliveryDays:7,
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -11,6 +11,7 @@ import { formatCurrency } from "../utils/money.js";
 import { deliveryOptions, getDeliveryOption,deliveryOptionsHTML,calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import {renderPaymentSummary} from './paymentSummary.js';
 import { renderCheckoutHeader } from "./checkoutHeader.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 export function renderOrderSummary(){
 let cartSummaryHTML = "";
@@ -21,7 +22,14 @@ cart.forEach((cartItem) => {
 
   const deliveryOption = getDeliveryOption(deliveryOptionsId);
   
-  const dateString =  calculateDeliveryDate(deliveryOption);
+  const today = dayjs();
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  );
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D'
+  );
   
   cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${
     matchingProduct.id
